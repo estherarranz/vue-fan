a156e0709fbea52f95627678d289542749e55df8
Nombre Apellido <alumno@gmail.com>
2026-03-25 17:46:27 +0100

    earrati: inicio de la app Tailwind,Shadcn y Vue router

---
+++ src/paginae/domus/Domus.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+
+</script>
+
+
+<template>
+
+<div class="cappa">
+  <h1>Home Page</h1>
+  
+  <Button variant="destructive">
+    Cilck me
+  </Button>
+</div>
+  
+</template>
+
+<style scoped>
+.cappa {
+  margin: 20px;
+}
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 12px;
+}
+</style>
