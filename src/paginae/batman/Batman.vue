3a893d6e87c7c5eacf4f95f06d931b27d801fc86
Nombre Apellido <alumno@gmail.com>
2026-04-01 18:32:44 +0200

    earrati:componentes carroussel y refactorización del scroll

--- src/paginae/batman/Batman.vue
+++ src/paginae/batman/Batman.vue
@@ -9,33 +9,13 @@ import {
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu'
 
-import { Card, CardContent } from '@/components/ui/card'
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from '@/components/ui/carousel'
+import Carrusimaginum from '@/components/ui/Carrusimaginum.vue';
 
-import Autoplay from 'embla-carousel-autoplay'
-
-const scrollToSection = (sectionId: string) => {
-   
-  if (sectionId === '#') {
-    window.scrollTo({ top: 0, behavior: 'smooth'})
-    return;
-  }
-
-  const element = document.querySelector<HTMLElement>(sectionId);
-
-    if (element) {
-      element?.scrollIntoView ({behavior: 'smooth', block: 'start'});
-    }
-}
+import { scrollToSection} from '@/utils/scrollToSection';
 
 
- const photos = ["justice", "arkham", "superman", "varios", "villana", "villano", "grupo", "robin", "anne", "joker", "resplandor", "cat", "gafas", "league", "fondoVerde"];
+
+const photos = ["justice", "arkham", "superman", "varios", "villana", "villano", "grupo", "robin", "anne", "joker", "resplandor", "cat", "gafas", "league", "fondoVerde"];
 
 </script>
 
@@ -133,39 +113,14 @@ const scrollToSection = (sectionId: string) => {
  </section>
 
 
- <section id="videre" class="w-full flex justify-center items-center min-h-[60vh] lg:min-h[95vh] bg-gray-900">
+ <section id="videre" class="w-full flex justify-center items-center min-h-[60vh] lg:min-h[95vh] bg-gray-900 text-white">
 
- <Carousel class="w-full max-w-md md:max-w-2xl lg:max-w-4xl bg-gray-900 text-white"
- :opts="{
-  dragFree:true,
-  loop:true,
- }"
- :plugins="[Autoplay({
-      delay: 2000,
-    })]"
- 
- >
-    <CarouselContent>
-      <CarouselItem v-for="i in photos.length" :key="i">
-        <div class="p-1">
-          <Card class="bg-gray-900 border-none">
-            <CardContent class="flex aspect-6/4 items-center justify-center p-6">
-              <img 
-              
-              :src="`/imagines/batman/${photos[i-1]}.jpg`" 
-              
-              :alt="`Imagen ${i-1} de Batman`"
-              
-              class="w-full h-full object-cover"
-              >
-            </CardContent>
-          </Card>
-        </div>
-      </CarouselItem>
-    </CarouselContent>
-    <CarouselPrevious class="hidden md:flex justify-center items-center" />
-    <CarouselNext class="hidden md:flex justify-center items-center"/>
-  </Carousel>
+  <Carrusimaginum
+  :photos="photos"
+  base-path="/imagines/batman"
+  :autoplay-delay="100"
+
+  />
 
  </section>
 
