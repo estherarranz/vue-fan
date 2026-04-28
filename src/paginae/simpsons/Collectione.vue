43314d6b9caffb910d1b96aeebe7d2f22b045938
estherarranz <estherarranzzz@gmail.com>
2026-04-28 17:17:36 +0200

    Traigo datos de los personajes para el grid

--- src/paginae/simpsons/Collectione.vue
+++ src/paginae/simpsons/Collectione.vue
@@ -1,13 +1,41 @@
 <script setup lang="ts">
 
+import { ref } from 'vue'; import CardContent from '@/components/ui/card/CardContent.vue';
+import { characters } from './Data';
+
+const listaSimpsons = ref( characters.slice(0, 6) )
+
+
 </script>
 
 
 <template>
     <div class="flex flex-col items-center justify-center w-full max-w-[1400px]">
 
-        <h1 class="font-bold text-xl mb-0.5">Simpsons Quote App</h1>
-        <p class="mb-5">Click on the quote of your favourite character</p>
+        <div class="text-center">
+
+            <h1 class="font-bold text-xl mb-0.5">Simpsons Quote App</h1>
+            <p class="mb-5">Click on the quote of your favourite character</p>
+
+        </div>
+
+        <div class="grid grid-cols-2 md:grid-cols-3 gap-8 mx-auto">
+
+            <Card 
+                v-for="character in listaSimpsons"
+                :key="character.id"
+                class="cursor-pointer w-[220px] h-[240px] hover:bg-[#ee3133] hover:text-white transition-colors"
+                >
+                <CardContent class="flex flex-col items-center gap-1 w-full px-0">
+
+                    <img 
+                    src="/imagines/simpsons/homer.png" alt=""
+                    class = "w-48 h-42 object-cover object-top mt-2 bg-amber-300 border border-b-blue-300">
+                    
+                    <h2>Homer Simpson</h2>
+                </CardContent>
+            </Card>
+        </div>
 
     </div>
 </template>
